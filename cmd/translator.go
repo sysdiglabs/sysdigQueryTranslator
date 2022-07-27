7f26fb9538bbefc92378a6f56401e87afd4d0644
David Lorite <david.lorite@sysdig.com>
2022-07-27 12:09:55 +0200

    Add the external package

--- cmd/translator.go
+++ cmd/translator.go
@@ -1,9 +1,7 @@
 package main
 
 import "C"
-import (
-	"regexp"
-)
+import . "github.com/sysdiglabs/sysdigQueryTranslator/pkg/translator"
 
 func main() {
 }
@@ -12,52 +10,7 @@ func main() {
 func RemoveSysdigLabelsInExpression(cExpr, cExcludeScope *C.char) *C.char {
 	expr := C.GoString(cExpr)
 	excludeScope := C.GoString(cExcludeScope)
-	// Remove scopes of sysdig kube_cluster_name=~$cluster for example
-	scope := "kube_cluster_name|kube_namespace_name|kube_pod_name|kube_workload_name|kube_node_name|agent_tag_cluster|instance|node|namespace|host_hostname"
-	if excludeScope != "" {
-		scope = scope + "|" + excludeScope
-	}
-	regex := regexp.MustCompile("(?:" + scope + ")" + "(?:=|=~|!=|!~)\\$[\\w_]*,")
-	expr = regex.ReplaceAllString(expr, "")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "(?:=|=~|!=|!~)\"\",")
-	expr = regex.ReplaceAllString(expr, "")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "(?:=|=~|!=|!~)\\$[\\w_]*\\}")
-	expr = regex.ReplaceAllString(expr, "}")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "(?:=|=~|!=|!~)\"\"}")
-	expr = regex.ReplaceAllString(expr, "}")
-	// Remove the sysdig aggregation
-	aggregation := "kube_cluster_name|kube_namespace_name|kube_pod_name|kube_workload_name|kube_node_name|agent_tag_cluster"
-	aggregation = aggregation + "|" + excludeScope
-	regex = regexp.MustCompile("(?:" + aggregation + ")")
-	expr = regex.ReplaceAllString(expr, "")
-	// Remove extra commas
-	regex = regexp.MustCompile("(?:,+)(?:\\s*)(?:,+)")
-	expr = regex.ReplaceAllString(expr, ",")
-	regex = regexp.MustCompile(",\\s*\\}")
-	expr = regex.ReplaceAllString(expr, "}")
-	regex = regexp.MustCompile("\\{\\s*,")
-	expr = regex.ReplaceAllString(expr, "{")
-	regex = regexp.MustCompile(",\\s*\\)")
-	expr = regex.ReplaceAllString(expr, ")")
-	regex = regexp.MustCompile("\\(\\s*,")
-	expr = regex.ReplaceAllString(expr, "(")
-	// Remove duplicate \\
-	regex = regexp.MustCompile("\\\\")
-	expr = regex.ReplaceAllString(expr, "")
-	// Remove empty by ()
-	regex = regexp.MustCompile("by\\s*\\(\\s*\\)")
-	expr = regex.ReplaceAllString(expr, "")
-	// Remove empty scope
-	regex = regexp.MustCompile("\\{\\s*\\}")
-	expr = regex.ReplaceAllString(expr, "")
-	// Remove 2 or more whitespaces
-	regex = regexp.MustCompile("[ \\t]{2,}")
-	expr = regex.ReplaceAllString(expr, " ")
-	// Remove the extra\n
-	regex = regexp.MustCompile("\\n")
-	expr = regex.ReplaceAllString(expr, "")
-	// Add trailing \n, very important don't touch
-	expr = expr + "\n"
+	expr = RemoveSysdigLabels(expr, excludeScope)
 	cStr := C.CString(expr)
 	return cStr
 }
