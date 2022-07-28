ce980e07aff1054a98985b0cc98aad45b89cbe4f
David Lorite <david.lorite@sysdig.com>
2022-07-28 12:06:49 +0200

    Add cross build and tests

--- example/translator.py
+++ example/translator.py
@@ -4,8 +4,8 @@ tr = ctypes.CDLL("../library/translator.so")
 translator = getattr(tr, "RemoveSysdigLabelsInExpression")
 translator.argtypes = [ctypes.c_char_p,ctypes.c_char_p]
 translator.restype = ctypes.c_char_p
-query = b"sum by (kube_cluster_name, input_namespace, input_container)(rate(fluentd_input_status_num_records_total{kube_cluster_name=~$cluster, input_namespace=~$input_namespace, input_container=~$input_container}[5m])) == 0"
-c_query = ctypes.c_char_p(query)
+query = 'sum by(kube_cluster_name,kube_namespace_name,kube_workload_name)(nginx_up{kube_cluster_name=~$cluster,kube_namespace_name  =~$namespace,kube_workload_name="delete-me", kube_pod_name!=""}) == 0'
+c_query = ctypes.c_char_p(bytes(query, "ascii"))
 empty = b""
 c_empty = ctypes.c_char_p(empty)
 returnVale = translator(c_query,c_empty)
