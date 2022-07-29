2b39ce070a7c7604ffb028c3e0036187fba773e1
David Lorite <david.lorite@sysdig.com>
2022-07-29 09:46:13 +0200

    Fix test

--- pkg/translator/translator_test.go
+++ pkg/translator/translator_test.go
@@ -9,7 +9,7 @@ var _ = Describe("Config", func() {
 	Context("When processing a text", func() {
 		It("Removes the Sysdig labels in expression", func() {
 			originalText := "sum by(kube_cluster_name,kube_namespace_name,kube_workload_name)\n(nginx_up{kube_cluster_name=~$cluster,kube_namespace_name  =~$namespace,kube_workload_name=\"delete-me\", kube_pod_name!=\"\"}) == 0\n"
-			expectedText := "sum (nginx_up) == 0\n"
+			expectedText := "sum (nginx_up) == 0"
 			processedText := RemoveSysdigLabels(originalText, "")
 			Expect(expectedText).To(Equal(processedText))
 		})
