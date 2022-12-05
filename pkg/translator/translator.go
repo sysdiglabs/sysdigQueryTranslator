2ac3e965b021c587267fd64f5f675844652d6414
David Lorite <david.lorite@sysdig.com>
2022-12-05 15:05:12 +0100

    Add all scope in the function

--- pkg/translator/translator.go
+++ pkg/translator/translator.go
@@ -3,19 +3,17 @@ package translator
 import "regexp"
 
 func RemoveSysdigLabels(expr, excludeScope string) string {
-	// Remove scopes of sysdig kube_cluster_name=~$cluster for example
-	scope := "kube_cluster_name|kube_namespace_name|kube_pod_name|kube_workload_name|kube_node_name|agent_tag_cluster|instance|node|namespace|host_hostname"
-	if excludeScope != "" {
-		scope = scope + "|" + excludeScope
+	regex := regexp.MustCompile("")
+	if excludeScope == "all" {
+		expr = RemoveAllScopes(expr, excludeScope)
+	} else {
+		// Remove scopes of sysdig kube_cluster_name=~$cluster for example
+		scope := "kube_cluster_name|kube_namespace_name|kube_pod_name|kube_workload_name|kube_node_name|agent_tag_cluster|instance|node|namespace|host_hostname"
+		if excludeScope != "" {
+			scope = scope + "|" + excludeScope
+		}
+		expr = RemoveAllScopes(expr, scope)
 	}
-	regex := regexp.MustCompile("(?:" + scope + ")" + "\\s*(?:=|=~|!=|!~)\\$[\\w_]*,")
-	expr = regex.ReplaceAllString(expr, "")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "\\s*(?:=|=~|!=|!~)\"[\\w_-]*\",")
-	expr = regex.ReplaceAllString(expr, "")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "\\s*(?:=|=~|!=|!~)\\$[\\w_]*\\}")
-	expr = regex.ReplaceAllString(expr, "}")
-	regex = regexp.MustCompile("(?:" + scope + ")" + "\\s*(?:=|=~|!=|!~)\"[\\w_-]*\"}")
-	expr = regex.ReplaceAllString(expr, "}")
 	// Remove the sysdig aggregation
 	aggregation := "kube_cluster_name|kube_namespace_name|kube_pod_name|kube_workload_name|kube_node_name|agent_tag_cluster"
 	aggregation = aggregation + "|" + excludeScope
@@ -48,5 +46,22 @@ func RemoveSysdigLabels(expr, excludeScope string) string {
 	regex = regexp.MustCompile("\\n")
 	expr = regex.ReplaceAllString(expr, "")
 	// Add trailing \n, very important don't touch
+	regex = regexp.MustCompile("\\[\\$__interval\\]")
+	expr = regex.ReplaceAllString(expr, "[5m]")
+	return expr
+}
+
+func RemoveAllScopes(expr, scope string) string {
+	if scope == "all" {
+		scope = "\\w"
+	}
+	regex := regexp.MustCompile("(?:" + scope + ")+" + "\\s*(?:=|=~|!=|!~)\\$[\\w_]*,")
+	expr = regex.ReplaceAllString(expr, "")
+	regex = regexp.MustCompile("(?:" + scope + ")+" + "\\s*(?:=|=~|!=|!~)\"[\\w_-]*\",")
+	expr = regex.ReplaceAllString(expr, "")
+	regex = regexp.MustCompile("(?:" + scope + ")+" + "\\s*(?:=|=~|!=|!~)\\$[\\w_]*\\}")
+	expr = regex.ReplaceAllString(expr, "}")
+	regex = regexp.MustCompile("(?:" + scope + ")+" + "\\s*(?:=|=~|!=|!~)\"[\\w_-]*\"}")
+	expr = regex.ReplaceAllString(expr, "}")
 	return expr
 }
--- pkg/translator/translator_test.go
+++ pkg/translator/translator_test.go
@@ -13,5 +13,17 @@ var _ = Describe("Config", func() {
 			processedText := RemoveSysdigLabels(originalText, "")
 			Expect(expectedText).To(Equal(processedText))
 		})
+		It("Remove all scopes", func() {
+			originalText := "irate(elasticsearch_indices_store_throttle_time_seconds_total{kube_cluster_name=$k8s_cluster,cluster=~$es_cluster, host=~$node,kube_namespace_name=~$namespace, kube_workload_name=~$workload}[$__interval]) "
+			expectedText := "irate(elasticsearch_indices_store_throttle_time_seconds_total{ }[$__interval]) "
+			processedText := RemoveAllScopes(originalText, "all")
+			Expect(expectedText).To(Equal(processedText))
+		})
+		It("Removes all labels", func() {
+			originalText := "irate(elasticsearch_indices_store_throttle_time_seconds_total{kube_cluster_name=$k8s_cluster,cluster=~$es_cluster, host=~$node,kube_namespace_name=~$namespace, kube_workload_name=~$workload}[$__interval]) "
+			expectedText := "irate(elasticsearch_indices_store_throttle_time_seconds_total[5m]) "
+			processedText := RemoveSysdigLabels(originalText, "all")
+			Expect(expectedText).To(Equal(processedText))
+		})
 	})
 })
